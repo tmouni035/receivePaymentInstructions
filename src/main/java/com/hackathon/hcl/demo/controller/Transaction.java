36fa7d1faff5ce97590f34f98a95bca27d56465a
mounikathirumani <mounikathirumani@mounikas-mbp.attlocal.net>
2021-02-27 16:18:43 -0600

    EntityClass

--- src/main/java/com/hackathon/hcl/demo/controller/Transaction.java
+++ src/main/java/com/hackathon/hcl/demo/controller/Transaction.java
@@ -2,29 +2,45 @@ package com.hackathon.hcl.demo.controller;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="transaction_tb1")
 public class Transaction {
 	
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int messageId;
+	@Column
 	private int endToEndId;
+	@Column
 	private String debtorAccount;
+	@Column
 	private String debtorName;
+	@Column
 	private String debtorAddress;
+	@Column
 	private String creditorAccount;
+	@Column
 	private String creditorName;
+	@Column
 	private String creditorAddress;
+	@Column
 	private int debtorAgentId;
+	@Column
 	private int creditorAgentId;
+	@Column
 	private String paymentCurrency;
+	@Column
 	private float paymentAmount;
+	@Column
 	private java.util.Date valueDate;
+	@Column
 	private String remittanceInfo;
 	
 	public Transaction() {
